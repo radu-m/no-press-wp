bd793db90f6f261ac7d4e4b82c3eddab4b17b43d
radu-m <radu.miu@gmail.com>
2017-08-14 05:18:00 +0200

    Initial theme setup

--- wp-content/themes/no-press-theme/functions.php
+++ wp-content/themes/no-press-theme/functions.php
@@ -457,3 +457,112 @@ function my_add_excerpts_to_pages()
     add_post_type_support('page', 'excerpt');
 }
 
+// from: https://clicknathan.com/web-design/automatically-create-pages-wordpress/
+// programmatically create some basic pages, and then set Home and Blog
+// setup a function to check if these pages exist
+function the_slug_exists($post_name) {
+    global $wpdb;
+    if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $post_name . "'", 'ARRAY_A')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// create the blog page
+if (isset($_GET['activated']) && is_admin()){
+    $blog_page_title = 'Blog';
+    $blog_page_content = 'This is blog page placeholder. Anything you enter here will not appear in the front end, except for search results pages.';
+    $blog_page_check = get_page_by_title($blog_page_title);
+    $blog_page = array(
+        'post_type' => 'page',
+        'post_title' => $blog_page_title,
+        'post_content' => $blog_page_content,
+        'post_status' => 'publish',
+        'post_author' => 1,
+        'post_slug' => 'blog'
+    );
+    // if(!isset($blog_page_check->ID) && !the_slug_exists('blog')){
+        $blog_page_id = wp_insert_post($blog_page);
+    // }
+}
+// create the site map page
+if (isset($_GET['activated']) && is_admin()){
+    $sitemap_page_title = 'Site Map';
+    $sitemap_page_check = get_page_by_title($sitemap_page_title);
+    $sitemap_page = array(
+        'post_type' => 'page',
+        'post_title' => $sitemap_page_title,
+        'post_status' => 'publish',
+        'post_author' => 1,
+        'post_slug' => 'site-map'
+    );
+    if(!isset($sitemap_page_check->ID) && !the_slug_exists('site-map')){
+        $sitemap_page_id = wp_insert_post($sitemap_page);
+    }
+}
+// change the Sample page to the home page
+if (isset($_GET['activated']) && is_admin()){
+    $home_page_title = 'Home';
+    $home_page_content = '';
+    $home_page_check = get_page_by_title($home_page_title);
+    $home_page = array(
+        'post_type' => 'page',
+        'post_title' => $home_page_title,
+        'post_content' => $home_page_content,
+        'post_status' => 'publish',
+        'post_author' => 1,
+        'ID' => 2,
+        'post_slug' => 'home'
+    );
+    if(!isset($home_page_check->ID) && !the_slug_exists('home')){
+        $home_page_id = wp_insert_post($home_page);
+    }
+}
+
+function enable_plugins() {
+
+    // Full path to WordPress from the root
+    $wordpress_path = '/full/path/to/wordpress/';
+
+    // Absolute path to plugins dir
+    $plugin_path = $wordpress_path.'wp-content/plugins/';
+
+    // Absolute path to your specific plugin
+    $my_plugin = $plugin_path.'my_plugin/my_plugin.php';
+
+    // Check to see if plugin is already active
+    if(is_plugin_active($my_plugin)) {
+
+        // Deactivate plugin
+        // Note that deactivate_plugins() will also take an
+        // array of plugin paths as a parameter instead of
+        // just a single string.
+        deactivate_plugins($my_plugin);
+    }
+    else {
+
+        // Activate plugin
+        activate_plugin($my_plugin);
+    }
+}
+
+if (isset($_GET['activated']) && is_admin()){
+    enable_plugins();
+   
+    // Set the blog page
+    $blog = get_page_by_title( 'Blog' );
+    update_option( 'page_for_posts', $blog->ID );
+
+
+    // Use a static front page
+    $front_page = 2; // this is the default page created by WordPress
+    update_option( 'page_on_front', $front_page );
+    update_option( 'show_on_front', 'page' );
+}
+
+// from: https://wordpress.stackexchange.com/questions/31207/how-to-set-permalink-structure-via-functions-php
+add_action( 'init', function() {
+    global $wp_rewrite;
+    $wp_rewrite->set_permalink_structure( '/%postname%/' );
+} );
